edadc380a42b2b3a3677bdb177d1c88f4de84ca4
lucie de hauteclocque <lucie.dehauteclocque@gmail.com>
2023-12-26 16:12:55 +0100

    routes and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,18 @@
 Rails.application.routes.draw do
+  root to: "pages#home"
+  resources :artworks
+  get "the_last_sunbeam/artwork", to: "artworks#the_last_sunbeam", as: :the_last_sunbeam
+  get "linogravures/artwork", to: "artworks#linogravures", as: :linogravures
+  get "instantanes/artwork", to: "artworks#instantanes", as: :instantanes
+  get "poemes/artwork", to: "artworks#poemes", as: :poemes
+end
+
+
+
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "open-uri"
+
+puts "Creating collections"
+Collection.create(title: "The Last Sunbeam") # 1
+Collection.create(title: "Linogravures") # 2
+Collection.create(title: "Instantanés") #3
+Collection.create(title: "Poèmes") # 4
+puts "Collections created"
+
+puts "Creating artwork"
+file_1 = URI.open("https://res.cloudinary.com/dni212b6h/image/upload/v1703221801/tls2_djty1u.jpg")
+artwork_1 = Artwork.new(title: "The Last Sunbeam n°1", dimension: "40x40cm", technique: "Acrylique on Canvas", price: 40.0, sold: false, collection_id:1)
+artwork_1.photo.attach(io: file_1, filename: "the_last_sunbeam_1.png", content_type: "image/png")
+artwork_1.save
+file_2 = URI.open("https://res.cloudinary.com/dni212b6h/image/upload/v1703221801/tls_4_lrdjhc.jpg")
+artwork_2 = Artwork.new(title: "The Last Sunbeam n°2", dimension: "40x40cm", technique: "Acrylique on Canvas", price: 40.0, sold: false, collection_id:1)
+artwork_2.photo.attach(io: file_2, filename: "the_last_sunbeam_2.png", content_type: "image/png")
+artwork_2.save
+file_3 = URI.open("https://res.cloudinary.com/dni212b6h/image/upload/v1703221801/tls_5_exdnd2.jpg")
+artwork_3 = Artwork.new(title: "The Last Sunbeam n°3", dimension: "40x40cm", technique: "Acrylique on Canvas", price: 40.0, sold: false, collection_id:1)
+artwork_3.photo.attach(io: file_3, filename: "the_last_sunbeam_3.png", content_type: "image/png")
+artwork_3.save
+puts "Artwork created"
